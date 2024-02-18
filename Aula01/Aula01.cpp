1c583d4dd33b4af1621f64cf208ff232ed4a864c
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-02-18 20:55:24 -0300

    Olá, Mundo em C++

--- Aula01/Aula01.cpp
+++ Aula01/Aula01.cpp
@@ -8,7 +8,8 @@ using namespace std;
 // main = significa que é a função principal a função main
 int main(){
   // cout = função que seria a "printf" do C, função para "escrever na tela"
-  cout << "Olá, Mundo em C++" << endl;
+  cout << "Olá, Mundo em C++" << endl; // endl para a quebra de linha ou \n
+  cout << "Teste de \n quebra de linha \n";
   //Fazer codigo esperar um entrada de tecla para finalizar
   system("pause");
   //Rertono "0"
