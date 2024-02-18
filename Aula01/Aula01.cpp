fdc413286e05973916c099744f1a223d6e59c3d0
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-02-18 20:45:02 -0300

    Olá mundo em C++

---
+++ Aula01/Aula01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// void = nulo ou seja a função não retorna nada
+// int = inteiro, função retorna um inteiro
+
+int main(){
+  // main = significa que é a função principal a função main
+  cout << "Olá, Mundo em C++";
+  system("pause");
+  return 0;
+}
