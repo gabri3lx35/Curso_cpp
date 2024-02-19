93092af1ab0094897952803a828131bb85346215
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-02-19 17:52:43 -0300

    Variaveis

--- Aula02/variaveis2.cpp
+++ Aula02/variaveis2.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 
+#define pi 3.1415
+#define ola cout << "Olá, Mundo" << "\n";
+
+
+
+
 int main(){
   //declaração de varias variaveis do mesmo tipo(int) de 1 linha.
-  int vidas, tiros, mana;
+  int vidas=3, tiros=100, mana=500;
+  cout << pi << "\n";
+  ola;
   cout << "Numero de vidas: ";
   cin >> vidas;
   cout << "Numero de mana: ";
@@ -11,6 +19,8 @@ int main(){
   cout << "Munição: ";
   cin >> tiros;
   cout << "FIM" << "\n";
+
+
   
   return 0;
 }
