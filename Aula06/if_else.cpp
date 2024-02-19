ced87de586850574253c911dffbb9e459cc15d9e
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-02-19 20:05:51 -0300

    if & else

---
+++ Aula06/if_else.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num=11;
+  char opc='s';
+
+/*se(teste lógico){
+  se o teste for verdadeiro
+}se não{
+  se o teste for falso
+}
+*/
+
+// > maior
+// < menor
+// >= maior ou igual 
+// <= menor ou igual
+// == igualdade
+// != diferente
+
+
+if(num == 10){
+  cout << "num é igual a 10 " << "\n";
+}else{
+  cout << "num não é igual a 10" << "\n";
+}
+
+  return 0;
+}
