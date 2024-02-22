53f11008d22415554b92e1a02126a491cb4889bc
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-02-21 23:05:26 -0300

    doWhile

---
+++ Aula12/DoWhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //doWhile, ele entra no bloco de codigo 1 vez pelo menos. mesmo que a condicão ja esteja satisfeita
+    int n1;
+    n1=20;
+
+    do{
+     cout << n1 << "\n";
+      n1++;
+    }while(n1<20);
+    
+
+
+
+  return 0;
+}
