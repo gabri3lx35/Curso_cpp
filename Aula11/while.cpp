a85508dfbba5f1817ab68b76c0354222e2f1ff8c
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-02-21 22:57:11 -0300

    while

---
+++ Aula11/while.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //While lanço de repetição, bom para quando não sabemos a condição de parada.
+    int n1;
+    n1=0;
+
+    while (n1<10)
+    {
+     n1++;
+     cout << n1 << "\n";
+    }
+    
+
+
+
+  return 0;
+}
