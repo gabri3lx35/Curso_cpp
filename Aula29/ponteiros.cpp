14561c80143dc39feb56de5f55c8b148ce7c0d2c
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-02-29 19:28:03 -0300

    ponteiros

--- Aula29/ponteiros.cpp
+++ Aula29/ponteiros.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 
 int main(){
-  
+  string veiculo="Carro";
+  string *pv;
+  pv=&veiculo; // & pega indereço da variavel e adiciona ela no ponteiro pv
+ //vantagem é que pode acessar o ponteiro de qualquer lugar/escopo
+  cout << &veiculo << "\n";
+  cout << pv << "\n";
+  *pv="moto"; // no endereço mencionado *pv, adiciona o valor moto. ou seja na variavel veiculo
+  cout << veiculo << "\n";
+ // um ponteiro armazena um endereço de outra variavel
+  return 0;
 }
