f260ad3d28400b4d6dcd4d729afb1570d499096a
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-02-29 23:05:32 -0300

    precision, scientific, setw e setfill

--- Aula32/complementadocout.cpp
+++ Aula32/complementadocout.cpp
@@ -13,10 +13,19 @@ float pi= M_PI;
 
 int num =10;
 
+cout.precision(3);//limitar variavel pi para 3 digiros 3.14 
 cout << "Valor de num em dec: " << num << "\n";
 cout << "Valor de num em hex: " << hex << num << "\n";
 cout << "Valor de num em oct: " << oct << num << "\n";
 cout << "Valor de num em hex: " << setbase(16) << num << "\n";
-
+cout << "Valor de pi: " << pi << "\n";
+cout.precision(-1);
+cout << "Valor de pi: " << pi << "\n";
+//valor de pi em notação cientifica
+cout << "Valor de pi: "<< std::scientific << pi << "\n";
+//adicionar uma largura
+cout << "Valor de pi: " << setw(10) << dec << num << "\n";
+//preencher um certo espaço com algum caractere
+cout << "Valor de pi: " << setw(10) << setfill('#') << dec << num << "\n";
   return 0;
 }
