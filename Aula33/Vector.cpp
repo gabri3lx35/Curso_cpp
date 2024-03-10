85fd1ea0cd4e5c42ff2d360ee826f645467da6a1
gabri3lx35 <110582782+gabri3lx35@users.noreply.github.com>
2024-03-10 15:03:22 -0300

    Aula33

--- Aula33/Vector.cpp
+++ Aula33/Vector.cpp
@@ -5,20 +5,33 @@ using namespace std;
 int main(){
 
   vector<int> num;
+  vector<int> num2;
   int tam,i;
 
   num.push_back(12);
   num.push_back(13);
   num.push_back(14);
-  
   num.push_back(15);
- for(i=0;i < 3; i++){
-   
+
+  num2.push_back(20);
+  num2.push_back(21);
+  num2.push_back(22);
+  num2.push_back(23);
+  
+  num.swap(num2); //troca inverte, valores de num2 foram para num
+
+  tam =num.size(); //tamanho
+
+  cout << tam << "\n";
+ for(i=0;i <= 3; i++){
+   cout << num[i] << "\n";
  }
 
-
-
-
+cout << num.front() << endl; //primeiro elemento
+cout << num.back() << endl; // ultimo elemento
+cout << num.at(2) << endl; //pegar elemento qualque lugar
+num.pop_back();
+num.clear(); //limpar os elementos
 
 
   return 0;
